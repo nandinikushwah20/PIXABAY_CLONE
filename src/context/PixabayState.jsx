c361b15368c4488684b60a1f727870bb1788431c
NandiniKushwah <nandinikushwah83@gmail.com>
2025-05-24 20:06:25 +0530

    PIXABAY_CLONE

---
+++ src/context/PixabayState.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import PixabayContext from './PixabayContext';
+
+const PixabayState = (props) => {
+    const [imageData, setImageData] = useState([]);
+    const [query, setQuery] = useState('london');
+
+    const api_key = '50474179-0e5a045179d54a314aa655505';
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const api = await fetch(
+                `https://pixabay.com/api/?key=${api_key}&q=${query}&image_type=photo&
+                pretty=true&per_page=100`
+            );
+            const data = await api.json();
+            setImageData(data.hits);
+            console.log(data.hits);
+        };
+        fetchData();
+    }, [query]);
+
+    const fetchImageByCategory = async (cat) => {
+        const api = await fetch(
+            `https://pixabay.com/api/?key=${api_key}&category=${cat}&
+            image_type=photo&pretty=true&per_page=100`
+        );
+        const data = await api.json();
+        setImageData(data.hits);
+        console.log(data.hits);
+    }
+
+    return (
+        <PixabayContext.Provider value={{ imageData, fetchImageByCategory, setQuery }}>
+            {props.children}
+        </PixabayContext.Provider>
+    )
+}
+
+export default PixabayState
